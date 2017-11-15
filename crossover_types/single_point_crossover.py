0d706fbed0552c39697f1ad123d27c77e139f2dc
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2017-11-15 21:57:02 +0530

    single point crossover example made simpler to understand

--- crossover_types/single_point_crossover.py
+++ crossover_types/single_point_crossover.py
@@ -14,8 +14,8 @@ def single_point_crossover(parent_1, parent_2, crossover_point):
             offspring_2.append(parent_1_dna)
     return [''.join(offspring_1), ''.join(offspring_2)]
 
-parent1 = '11101001000'
-parent2 = '00001010101'
+parent1 = '11111111111'
+parent2 = '00000000000'
 offsprings = single_point_crossover(parent1, parent2, 5)
 print("\n Single point crossover")
 print("Parent 1    : " + parent1)
